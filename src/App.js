3e084cbd171f6e289bb004c40b689123e774efa4
abdullah0o0 <abdullah.syallah0@gmail.com>
2021-07-01 15:14:40 +0200

    create rows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import React from 'react';
+import requests from './requests'
+import Row from './Row';
+import Banner from './'
 
 function App() {
   return (
     <div className="App">
-      <h1>whats up guys!</h1>
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHerrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
