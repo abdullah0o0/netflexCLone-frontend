cef18caf0d63e530128ebac1d96cb3d6ea9b15ab
abdullah0o0 <abdullah.syallah0@gmail.com>
2021-07-02 15:05:28 +0200

    add navbar +navStyles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,7 @@ import '../assets/Nav.scss'
 export default function Navbar() {
     return (
         <div className="nav" >
-            <img className="nav_logo" src="https://www.xda-developers.com/files/2019/03/Netflix-Logo.png
-" 
+            <img className="nav_logo" src="https://cloudfront-us-east-1.images.arcpublishing.com/gray/3HCWZMP7PFGY3OJJPFHIX5O2VI.png" 
             alt="Netflix Logo" />
 
             <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix Logo" />
