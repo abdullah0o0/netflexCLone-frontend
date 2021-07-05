9d8b2a04299ad5307c06eab5dede98336aa6644e
abdullah0o0 <abdullah.syallah0@gmail.com>
2021-07-05 12:25:21 +0200

    add trailer

--- NetflixClone-frontend/src/components/Navbar.js
+++ NetflixClone-frontend/src/components/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/Nav.scss'
 
 export default function Navbar() {
+    const [show,handleShow] = useState(false)
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY > 100){
+                handleShow(true)
+            }else{
+                handleShow(false)
+            }
+        })
+        return () =>{
+            window.removeEventListener("scroll")
+        }
+    },[])
     return (
-        <div className="nav" >
+        <div className={`nav ${show && "nav_black"}`} >
             <img className="nav_logo" src="https://cloudfront-us-east-1.images.arcpublishing.com/gray/3HCWZMP7PFGY3OJJPFHIX5O2VI.png" 
             alt="Netflix Logo" />
 
